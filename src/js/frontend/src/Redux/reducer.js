4a585f2bec2a57a676201b190f78872bc4fdb1ef
Adam Spiers <hackbrexit@adamspiers.org>
2017-01-24 13:49:22 +0000

    ui: add new ADD_ENTITY Redux action
    This will be used for caching data (e.g. the name) of a given entity.

--- src/js/frontend/src/Redux/reducer.js
+++ src/js/frontend/src/Redux/reducer.js
@@ -8,8 +8,10 @@ import * as actionTypes from "./action-types";
 // http://redux.js.org/docs/api/combineReducers.html
 
 export function rootReducer (state = IMap(), action) {
+  let foo = state.get("showAboutScreen", undefined);
   return state.merge({
-    showAboutScreen: aboutReducer(state.get("showAboutScreen", undefined), action),
+    showAboutScreen: aboutReducer(foo, action),
+    entities: addEntityReducer(state.get("entities"), action),
   })
   return state;
 }
@@ -23,14 +25,38 @@ export function rootReducer (state = IMap(), action) {
 
 const defaultState = {
   showAboutScreen: false,
+  entities: {
+    people: {},
+    organisations: {},
+    "government-offices": {},
+  },
 };
 
-export function aboutReducer(state = defaultState, {type}) {
+export function aboutReducer(state = defaultState.showAboutScreen, {type}) {
   switch (type) {
     case actionTypes.TOGGLE_ABOUT:
       let newState = Object.assign({}, state);
       newState.showAboutScreen = !newState.showAboutScreen;
       return newState;
+    default:
+      return state;
+  }
+}
+
+export function addEntityReducer(state = defaultState.entities, action) {
+  let {type, entityType, id, data} = action;
+
+  switch (type) {
+    case actionTypes.ADD_ENTITY:
+      if (! state.has(entityType)) {
+        console.error(`Received ADD_ENTITY action with invalid entity type ${entityType}`);
+        return state;
+      }
+      return state.mergeDeep({
+        [entityType]: {
+          [id]: data
+        },
+      });
     default:
       return state;
   }
